b316bf9dcb7be9778ef12a547d75765aa637c36d
Abilesh-S <145469513+Abilesh-S@users.noreply.github.com>
2025-09-07 15:04:32 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
-import pytest
+# flake8: noqa: E402
 import sys
 import os
+
+# Add project root to PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
-
+import pytest
 
 @pytest.fixture
 def client():
