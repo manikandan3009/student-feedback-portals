21aa274e590b29a9aa9c3c9a53275e82fb692836
Abilesh-S <145469513+Abilesh-S@users.noreply.github.com>
2025-09-07 14:58:28 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
-
 import pytest
-from app import app as flask_app
+from app import app
+
 
 @pytest.fixture
 def client():
-    flask_app.config['TESTING'] = True
-    with flask_app.test_client() as client:
+    with app.test_client() as client:
         yield client
